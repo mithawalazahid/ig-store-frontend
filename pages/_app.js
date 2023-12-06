1437b69f7d1b0eab000cf64f7c49cb85e383dfb2
mithawalazahid <145277904+mithawalazahid@users.noreply.github.com>
2023-12-06 22:44:40 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '@/styles/globals.css'
 import Head from 'next/head'
-import Header from '@/Components/Header';
+import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
 import store from '@/store/store';
